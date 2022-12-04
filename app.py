d0658dc205fa99247615524d69247c19a30aae49
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2022-12-04 23:36:58 +0200

    Update requirements

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL") #.replace("://", "ql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL").replace("://", "ql://", 1)
 app.secret_key = os.getenv("SECRET_KEY")
 db = SQLAlchemy(app)
 
