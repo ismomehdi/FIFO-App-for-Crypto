12b1db332b796f99e2aee5c73f6d6470f7650d26
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2022-12-18 18:25:21 +0200

    Modify db url for fly

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
-    'DATABASE_URL')  # .replace('://', 'ql://', 1)
+    'DATABASE_URL').replace('://', 'ql://', 1)
 app.secret_key = os.getenv('SECRET_KEY')
 db = SQLAlchemy(app)
 
